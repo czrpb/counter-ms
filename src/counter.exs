1c73e45d50771bed22d3f67c4b0e7b9191af7bb7
czrpb <czrpb@yahoo.com>
2022-05-22 15:25:13 -0700

    wrapped gen_tcp

--- src/counter.exs
+++ src/counter.exs
@@ -1,46 +1,80 @@
-defmodule CounterMicroservice do
-  def start(port) do
-    IO.puts("Serving on port #{port}...")
+defmodule CleanGenTcp do
+  def listen(port) do
+    IO.puts("[GEN_TCP] Listening...")
     {:ok, socket} = :gen_tcp.listen(
       port,
       active: false, packet: :http_bin, reuseaddr: true
     )
-
-    serve(socket, 0)
+    fn ->
+      IO.puts("[GEN_TCP] Accepting...")
+      {:ok, conn} = :gen_tcp.accept(socket)
+      conn(conn)
+    end
   end
 
-  def serve(socket, counter) do
-    IO.puts("Waiting for a connection, current counter is #{counter}...")
-    {:ok, conn} = :gen_tcp.accept(socket)
+  def conn(conn) do
+    {
+      {
+        fn ->
+          IO.puts("[GEN_TCP] Receiving...")
+          :gen_tcp.recv(conn, 0)
+        end,
+        fn response ->
+          IO.puts("[GEN_TCP] Sending...")
+          :gen_tcp.send(conn, response)
+        end
+      },
+      fn ->
+        IO.puts("[GEN_TCP] Closing...")
+        :gen_tcp.close(conn) end
+    }
+  end
+end
 
-    reply(:gen_tcp.recv(conn, 0), conn, counter)
-    :gen_tcp.close(conn)
+defmodule CounterMicroservice do
+  def start(port, counter \\ 0) do
+    IO.puts("[#{counter}] Binding on port #{port}...")
+    port
+    |> CleanGenTcp.listen
+    |> serve(counter)
+  end
 
-    serve(socket, counter+1)
+  def serve(accept, counter) do
+    IO.puts("[#{counter}] Serving...")
+    {conn, close} = accept.()
+
+    {recv, _} = conn
+
+    spawn(fn ->
+      reply(recv.(), conn, counter)
+      close.()
+    end)
+
+    serve(accept, counter+1)
   end
 
   def reply(
     {:ok, {:http_request, :GET, {:abs_path, "/"}, _}},
-    conn,
+    {recv, _}=conn,
     counter
   ) do
-    IO.puts("Got a connection...")
-    reply(:gen_tcp.recv(conn, 0), conn, counter)
+    IO.puts("[#{counter}] Got HTTP GET...")
+    reply(recv.(), conn, counter)
   end
 
-  def reply({:ok, :http_eoh}, conn, counter) do
-    IO.puts("End of headers, responding to request counter of #{counter}...")
+  def reply({:ok, :http_eoh}, {_, send}, counter) do
     response = """
     HTTP/1.1 200 OK
 
     #{counter}
     """
-    :gen_tcp.send(conn, response)
+    IO.puts("[#{counter}] HTTP request finished, replying...")
+    send.(response)
   end
 
-  def reply({:ok, _}, conn, counter) do
-    IO.puts("Ignoring other parts of the HTTP protocol...")
-    reply(:gen_tcp.recv(conn, 0), conn, counter)
+  def reply({:ok, _}, {recv, _}=conn, counter) do
+    IO.puts("[#{counter}] Ignoring HTTP header...")
+    reply(recv.(), conn, counter)
   end
 end
 
